a7a80db2f9495c78a97daacc9416391a6fe84ab6
Jack Bransfield <jjbii@builtlight.org>
2020-07-09 02:46:19 -0500

    Remove main storyboard and configure window and root VC.

---
+++ Top 100/Top 100/AppFlowController.swift
@@ -0,0 +1,30 @@
+//
+//  AppFlowController.swift
+//  Top 100
+//
+//  Created by Jack Bransfield on 7/9/20.
+//  Copyright © 2020 Built Light. All rights reserved.
+//
+
+import UIKit
+
+class AppFlowController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
